21060c6b40684715fe0186ba7ba316a0ca90ddb7
Alex M <mr9891549@gmail.com>
2025-02-17 15:47:18 +0300

    Сделал Лр 1 и Лр 2

---
+++ LR-1/Code/LR_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+string buf; // Переменная для временного хранения введённых значений
+
+// Ввод значения x
+Console.Write("Введите x: ");
+buf = Console.ReadLine(); // Читаем ввод пользователя в виде строки
+double x = Convert.ToDouble(buf); // Преобразуем строку в число (double)
+
+// Ввод значения y
+Console.Write("Введите y: ");
+buf = Console.ReadLine();
+double y = Convert.ToDouble(buf);
+
+// Ввод значения z
+Console.Write("Введите z: ");
+buf = Console.ReadLine();
+double z = Convert.ToDouble(buf);
+
+// Вывод введённых значений
+Console.WriteLine($"Для x={x}; y={y}; z={z}");
+
+// Вычисление выражения a:
+// (1 + y)^2 * ( (x^2 + 4) / (e^(-x) + x^2 + 4) )
+double a = Math.Pow((1 + y), 2) * ((Math.Pow(x, 2) + 4) / (Math.Exp(-x) + Math.Pow(x, 2) + 4));
+
+Console.WriteLine($"Результат a = {a}");
+
+// Вычисление выражения b:
+// 1 / ( (x^4)/2 + sin^4(z) + 1 )
+double b = 1 / (((Math.Pow(x, 4)) / 2) + Math.Pow(Math.Sin(z), 4) + 1);
+
+Console.WriteLine($"Результат b = {b}");
---
+++ LR-2/LR_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Для выхода из цикла нажмите '0'");
+        Console.WriteLine("Вводите символы для проверки, к какому алфавиту они относятся.");
+
+        while (true)
+        {
+            Console.Write("Введите символ: ");
+            string buf = Console.ReadLine();
+
+            if (buf == "0") // Проверяем, ввёл ли пользователь '0' для выхода
+            {
+                Console.WriteLine("Выход из программы");
+                break;
+            }
+
+            if (buf.Length != 1) // Проверяем, что введён только один символ
+            {
+                Console.WriteLine("Ошибка: Введите только один символ.");
+                continue;
+            }
+
+            char a = char.ToLower(buf[0]); // Приводим к нижнему регистру
+
+            switch (a)
+            {
+                default:
+                    if ("abcdefghijklmnopqrstuvwxyz".Contains(a))
+                        Console.WriteLine("Символ из латинского алфавита");
+                    else if ("абвгдеёжзийклмнопрстуфхцчшщъыьэюя".Contains(a))
+                        Console.WriteLine("Символ из русского алфавита");
+                    else
+                        Console.WriteLine("Символ не из латинского и не из русского алфавита");
+                    break;
+            }
+        }
+    }
+}
