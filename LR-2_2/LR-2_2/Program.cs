e000c451e9bcee3f26bd8e8fc518a8233f311cc2
Alex M <mr9891549@gmail.com>
2025-02-18 10:48:30 +0300

    Сделал 2-е задание из Лр 2

---
+++ LR-2_2/LR-2_2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите последовательность чисел для того что-бы программа могла удалить числа 1 и 3");
+string input = Console.ReadLine();
+
+Console.WriteLine("Последовательность чисел после удаления:");
+string result = input.Replace("1", "").Replace("3","");
+Console.WriteLine(result);
